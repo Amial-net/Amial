a4a329736c5ce11514081f2328014ba760178bcb
wangv69 <viiwang69@gmail.com>
2026-02-10 16:08:14 -0500

    Added an interactable text box, made the enter box a button, and fixed the spacing.

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,22 +1,20 @@
 {/* To do:
     - Background Image
-    - Text Box
-    - Logo
-    
+    - Logo Image
 */}
-
-
 
 export default async function LoginPage() {
     return <div>
-        <h1 className="px-50 text-7xl underline font-bold"> Amial </h1>
+        <h1 className="px-50 text-7xl underline font-bold my-10"> Amial </h1>
         <div className="bg-black"></div> 
 
         <h1 className="px-50 text-3xl mt-30 font-bold"> Login </h1>
-        <h1 className="px-50 text-1xl mt-2"> Username </h1>
-        {/* going to make a text box */}
+        <h1 className="px-50 text-1xl mt-3"> Username </h1>
+        <input type="text" placeholder="username" className="px-50 text-1xl mt-3"></input>
         <h1 className="px-50 text-1xl mt-5"> Password </h1>
-        {/* going to make a text box */}
+        <input type="text" placeholder="password" className="px-50 text-1xl mt-3"></input>
+
+        <button className="mt-10 mx-50 px-40 py-2.5 bg-white text-black hover:bg-slate-600 flex items-center ">Enter</button>
 
     </div>
 }
