03f98b5fdf3feca6d3b0e9ccce890645f9d56137
Miguel Santiago <gameeureka2@gmail.com>
2026-02-15 23:38:25 -0500

    message page

---
+++ client/src/Pages/MessagePage.tsx
@@ -0,0 +1,39 @@
+export default function MessagePage() {
+  return (
+    <>
+      <SideBar />
+      <MainChatArea />
+      <MessagesPanel />
+    </>
+  );
+}
+
+function SideBar() {
+  return <></>;
+}
+
+function MainChatArea() {
+  return <></>;
+}
+
+function MessagesPanel() {
+  return (
+    <>
+      <MessagesHeader />
+      <ConversationList />
+      <SearchBar />
+    </>
+  );
+}
+
+function MessagesHeader() {
+  return <></>;
+}
+
+function ConversationList() {
+  return <></>;
+}
+
+function SearchBar() {
+  return <></>;
+}
