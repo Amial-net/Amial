2589a686870dd97203752382d3951be9f8600a10
wangv69 <viiwang69@gmail.com>
2026-02-23 16:49:58 -0500

    Added a varible to hold the value of the inputs, also added a log to see the email and password when the button is pressed

--- client/src/Pages/LoginPage.tsx
+++ client/src/Pages/LoginPage.tsx
@@ -1,7 +1,6 @@
 {/* To do:
     - Logo/Background Image (waiting for group leader)
     - Button to actually show the password
-    - Button to show something when button is pressed
 */}
 
 import {useState} from "react";
@@ -14,30 +13,34 @@ export default function LoginPage() {
     )
 }
 
-function enterButton(){
+function enterButton(email:string, password:string){
     console.log("Button clicked");
+    console.log(email);
+    console.log(password);
 }
 
 function Login(){
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
     return (
         //temporary until I can make it into a cleaner look 
         <div className="bg-[url(/hero.png)] min-h-screen bg-no-repeat bg-cover bg-center gap-8 max-w-full h-[80vh] text-shadow-lg text-shadow-zinc-500">
-        
+
             <h1 className="text-7xl underline font-bold my-10"> Amial </h1>
             <div className="px-50 text-3xl mt-30 font-bold"> Login </div>
 
-
             <div className="px-50 text-1xl mt-3"> Email </div>
-            <input type="text" placeholder="Email" className="mx-50 text-1xl mt-3"></input>
+            <input type="text" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} className="mx-50 text-1xl mt-3"></input>
 
 
             <div className="px-50 text-1xl mt-5"> Password </div>
-            <input type="password" placeholder="Password" className="mx-50 text-1xl mt-3"></input>
+            <input type="password" placeholder="Password" value={password} onChange = {(e) => setPassword(e.target.value)} className="mx-50 text-1xl mt-3"></input>
 
-
+            
 
             <button className="mt-10 mx-50 px-40 py-2.5 bg-black text-white hover:bg-slate-600 flex items-center active:bg-blue-600"
-                onClick={enterButton}
+                onClick={() => enterButton(email, password)}
             >
                 Enter
             </button>
