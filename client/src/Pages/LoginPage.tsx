fe28fe1c7cd1501c018fe81875c3c3ef3189bdf1
Miguel Santiago <gameeureka2@gmail.com>
2026-02-27 22:35:35 -0500

    MessagePage first design complete

--- client/src/Pages/LoginPage.tsx
+++ client/src/Pages/LoginPage.tsx
@@ -1,49 +1,43 @@
-{/* To do:
-    - Logo/Background Image (waiting for group leader)
-    - Button to actually show the password
-*/}
-
-import {useState} from "react";
-
-export default function LoginPage() {
-    return (
-        <div className="">
-            <Login/>
-        </div>
-    )
+export default function LandingPage() {
+  return (
+    <div>
+      <Toolbar />
+      <HeroImage />
+    </div>
+  );
 }
 
-function enterButton(email:string, password:string){
-    console.log("Button clicked");
-    console.log(email);
-    console.log(password);
+function Toolbar() {
+  const links = [
+    ["Home", "#"],
+    ["Resources", "#"],
+    ["Contact", "/messages"],
+    ["Sign in", "/login"],
+    ["Mission", "#"],
+  ];
+  return (
+    <div className="max-w-full flex [&>div]:p-5">
+      <div>Amial</div>
+      <div className="flex-auto" />
+      {links.map((i) => {
+        return (
+          <a href={i[1]} className="p-5" key={i[0]}>
+            {i[0]}
+          </a>
+        );
+      })}
+    </div>
+  );
 }
 
-function Login(){
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-
-    return (
-        //temporary until I can make it into a cleaner look 
-        <div className="bg-[url(/hero.png)] min-h-screen bg-no-repeat bg-cover bg-center gap-8 max-w-full h-[80vh] text-shadow-lg text-shadow-zinc-500">
-
-            <h1 className="text-7xl underline font-bold my-10"> Amial </h1>
-            <div className="px-50 text-3xl mt-30 font-bold"> Login </div>
-
-            <div className="px-50 text-1xl mt-3"> Email </div>
-            <input type="text" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} className="mx-50 text-1xl mt-3"></input>
-
-
-            <div className="px-50 text-1xl mt-5"> Password </div>
-            <input type="password" placeholder="Password" value={password} onChange = {(e) => setPassword(e.target.value)} className="mx-50 text-1xl mt-3"></input>
-
-            
-
-            <button className="mt-10 mx-50 px-40 py-2.5 bg-black text-white hover:bg-slate-600 flex items-center active:bg-blue-600"
-                onClick={() => enterButton(email, password)}
-            >
-                Enter
-            </button>
-        </div>
-    )
+function HeroImage() {
+  return (
+    <div className="bg-[url(/hero.png)] bg-no-repeat bg-cover bg-center gap-8 max-w-full h-[80vh] flex flex-col items-center justify-center text-black text-shadow-lg text-shadow-zinc-500 font-black">
+      <div className="text-9xl text-center">Amial</div>
+      <div className="text-5xl">Meet new people in your free time</div>
+      <button className="mt-3 text-3xl px-6 py-2 bg-zinc-200 rounded-sm dark:bg-zinc-800 dark:text-white">
+        Join the Waitlist
+      </button>
+    </div>
+  );
 }
