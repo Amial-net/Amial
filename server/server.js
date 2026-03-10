04874f4edceea4ee23a7816d34ef814d2a872b5b
sxyg1r <143647406+sxyg1r@users.noreply.github.com>
2026-03-09 20:23:03 -0400

    Created User Session to 1 day and finalized User route

--- server/server.js
+++ server/server.js
@@ -2,13 +2,17 @@ const express = require("express");
 const cors = require("cors");
 const session = require("express-session");
 const { connectDB } = require("./scripts/seed");
+const authRoutes = require("./routes/UserRoutes");
 
 const app = express();
 port = 3000;
 
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(
+  cors({
+    origin: "http://localhost:5173", //front end could be different on deployment
+    credentials: true,
+  })
+);
 
 connectDB()
     .then(() => {
@@ -19,6 +23,27 @@ connectDB()
     .catch((err) =>{
         console.error("Database connection failed. Server not started.", err);
     })
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  session({
+    secret: "replace_this_with_a_real_secret",
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      httpOnly: true,
+      secure: false, // true only when using HTTPS
+      sameSite: "lax",
+      maxAge: 1000 * 60 * 60 * 24, // 1 day
+    },
+  })
+);
+
+app.use("/auth", authRoutes);
+
+
 
 
     
