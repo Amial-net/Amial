a17286dc0c718a1a9bd24cf67793e8cfda85f7b0
wangv69 <viiwang69@gmail.com>
2026-04-21 12:55:20 -0400

    Fixed login page and removed redundant functions

--- client/src/Pages/LoginPage.tsx
+++ client/src/Pages/LoginPage.tsx
@@ -4,7 +4,8 @@ import { Link, useNavigate } from "react-router";
 export default function LoginPage() {
   return (
     <div>
-      <Login />
+      <Toolbar/>
+      <Login/>
     </div>
   );
 }
@@ -100,13 +101,10 @@ function Login() {
             <span>Sign up</span>
             <span className="text-blue-600"> here</span>
           </Link>
-    return (
-        <div className="">
-            <Toolbar/>
-            <Login/>
-        </div>
-    )
-}
+      </div>
+    </div>
+    </div>
+)}
 
 function Toolbar() {
   const links = [
@@ -128,59 +126,6 @@ function Toolbar() {
           </a>
         );
       })}
-    </div>
-  );
-}
-
-function enterButton(email:string, password:string){
-    console.log("Button clicked");
-    console.log(email);
-    console.log(password);
-}
-
-function Login(){
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [showPassword, setShowPassword] = useState(false); 
-
-    return (
-        <div className="bg-black min-h-screen bg-no-repeat bg-cover bg-center">
-            <h1 className="text-7xl underline font-bold text-white flex flex-col items-center gap-1 align-center "> Amial </h1>
-            <div className="flex justify-center items-center min-h-screen">
-                <div className="bg-white h-[90%] min-h-[40vh] min-w-[70vh] max-w-xs w-full border rounded-[10%] flex flex-col items-center gap-1 align-center content-center justify-center">
-
-                    <div className="text-center text-3xl font-bold"> Login </div>
-
-                    <div className="text-1xl text-left w-full pl-[5%]"> Email or Username</div>
-                    <div className="text-left w-full pl-[5%]">
-                        <input type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} className="text-1xl w-[95%]"></input>
-                    </div>
-                    
-                    <div className="text-1xl text-left w-full pl-[5%]"> Password </div>
-                    <div className="flex text-left w-full pl-[5%]">    
-                        <input type={showPassword ? "text" : "password"} placeholder="Password" value={password} onChange = {(e) => setPassword(e.target.value)} className="text-1xl w-[95%]"></input>
-                        <button className="ml-auto bg-[url(/show-password-icon.png)] bg-no-repeat bg-center bg-contain pl-[5%] text-white hover:bg-slate-600"
-                                onClick={() => setShowPassword((prev) => !prev)}>
-                        </button>
-                    </div>
-
-                    <button className="px-[42%] bg-black text-white hover:bg-slate-600 flex items-center active:bg-blue-600"
-                        onClick={() => enterButton(email, password)} >
-                        Enter
-                    </button>
-
-                    <p className="">
-                        Dont have an account?
-                    </p>   
-
-                    <Link to="/signup" className="">
-                        <span>Sign up</span>
-                        <span className="text-blue-600"> here</span> 
-                    </Link>
-                </div>
-            </div>
-        </div>
-      </div>
     </div>
   );
 }
