acc35167dd969cdf8b4f2b71ef32a9ac4471be89
Richard Wei <Richardwei4174@gmail.com>
2026-04-25 16:00:28 -0400

    Add Calendar page layout and route

--- client/src/Pages/CalendarPage.tsx
+++ client/src/Pages/CalendarPage.tsx
@@ -1,4 +1,4 @@
-import MainSideBar from "../components/SideBar";
+import MainSideBar from "../components/SideBar.tsx";
 
 export default function CalendarPage() {
   return (
@@ -9,11 +9,37 @@ export default function CalendarPage() {
         <MainSideBar />
       </div>
 
-      {/* RIGHT CONTENT (calendar will go here) */}
-      <div className="col-span-9 p-6">
-        <h1 className="text-3xl font-bold">Calendar</h1>
+      {/* CENTER CALENDAR */}
+      <div className="col-span-6 border-r border-black/10 bg-white">
+
+        {/* CALENDAR HEADER */}
+        <div className="p-6 border-b border-black/10">
+          <h1 className="text-[24px] font-extrabold">Calendar</h1>
+          <p className="text-[14px] text-black/40">
+            Plan your moments and keep track of meaningful connections.
+          </p>
+        </div>
+
+        {/* CALENDAR BODY */}
+        <div className="p-4">
+          <div className="rounded-[18px] border border-black/10 p-5">
+            Calendar goes here
+          </div>
+        </div>
+
       </div>
 
+      {/* RIGHT SIDEBAR */}
+      <div className="col-span-3 bg-white p-4">
+
+        <div className="rounded-[18px] border border-black/10 p-5">
+          <h3 className="text-[18px] font-extrabold mb-4">Today</h3>
+          <p className="text-[14px] text-black/60">
+            No events yet.
+          </p>
+        </div>
+
+      </div>
     </div>
   );
 }
